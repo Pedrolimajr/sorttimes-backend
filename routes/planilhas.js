08f02c0078a6c8480e52b5d80db8e7ff1541a6f1
Pedrolimajr <pedrosouzalima@hotmail.com>
2025-05-28 15:44:05 -0300

    Correção em planilhas

--- routes/planilhas.js
+++ routes/planilhas.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const Planilha = require('../models/Planilha');
 // Middleware para headers CORS
 router.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();

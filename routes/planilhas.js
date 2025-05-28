78a52008b9fdad437f1b939cba2aeb8d75b07985
Pedrolimajr <pedrosouzalima@hotmail.com>
2025-05-28 13:32:28 -0300

    Remoção do module router

--- routes/planilhas.js
+++ routes/planilhas.js
@@ -104,3 +104,5 @@ router.put('/:id', async (req, res) => {
     });
   }
 });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -125,4 +125,4 @@ const startServer = async () => {
   });
 };
 
-module.exports = router;
+
